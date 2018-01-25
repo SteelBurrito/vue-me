98576889c7cc4a04ec2b1133d9bda9a8dd2f193e
SteelBurrito <antouniokevin@gmail.com>
2018-01-25 12:00:55 +0700

    setting up scss, single page layout

---
+++ src/components/me/Header.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class='menu-container blue-gradient'>
+        <div class="page-heading">
+            <div class='page-title'>
+                <h1>bloggo.club</h1>
+            </div>
+            <div class="page-description">
+                <p>Personal webpage managed by Antonio Kevin Christophorus</p>
+            </div>
+        </div>
+    </div>
+    <!-- <component v-bind:is="renderDescription"></component> -->
+</template>
+
+<script>
+export default {
+  data() {
+      return {
+          
+      }
+  },
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+import HelloWorld from '@/components/me/HelloWorld';
+import Header from '@/components/me/Header';
 
 Vue.use(Router);
 
@@ -8,6 +9,11 @@ export default new Router({
   routes: [
     {
       path: '/',
+      name: 'Header',
+      component: Header,
+    },
+    {
+      path: '/test',
       name: 'HelloWorld',
       component: HelloWorld,
     },
